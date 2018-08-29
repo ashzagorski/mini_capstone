4949fb31013018439b20540abc791391a0735453
ashzagorski <ash.zagorski@gmail.com>
2018-08-28 20:28:14 -0500

    added two individual product route/controller/view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
     render 'all_products_view.json.jbuilder'
   end 
 
-  def product_action
-    render 'product.json.jbuilder'
+  def first_product_action
+    @product = Product.first
+    render 'first_product_view.json.jbuilder'
+  end 
+
+  def second_product_action
+    @product = Product.find_by(id: 2)
+    render 'second_product.json.jbuilder'
   end 
 end
